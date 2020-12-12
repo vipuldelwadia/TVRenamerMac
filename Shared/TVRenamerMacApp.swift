75fef113046e9b4d989e0aebf5fee495726c4fae
Vipul Delwadia <vipul.delwadia@gmail.com>
2020-12-12 21:05:37 +1300

    Brand new project, literally no changes.

---
+++ Shared/TVRenamerMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVRenamerMacApp.swift
+//  Shared
+//
+//  Created by Vipul Delwadia on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct TVRenamerMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
